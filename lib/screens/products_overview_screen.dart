95513d88be444456292f928008b839b02cdb43a2
Mark-DSouza <markdsouza434@gmail.com>
2021-04-22 16:11:52 +0530

    Add ProductsOverviewScreen and product model

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Products Overview'),
+        ),
+        body: Center(
+          child: Text('Products overview body'),
+        ));
+  }
+}
