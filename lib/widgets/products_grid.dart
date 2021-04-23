657b46d82c37ae9052963940023e3a50f03bbd3a
Mark-DSouza <markdsouza434@gmail.com>
2021-04-23 10:15:12 +0530

    Add products Provider and hook it up

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) {
+        return ProductItem(
+          id: products[index].id,
+          title: products[index].title,
+          imageUrl: products[index].imageUrl,
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+      ),
+    );
+  }
+}
